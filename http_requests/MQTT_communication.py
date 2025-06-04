d7eec5d18112b17abafc7f7b3665042a8cf2cefd
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-04 06:39:19 +0000

    Edit MQTT_communication.py

--- http_requests/MQTT_communication.py
+++ http_requests/MQTT_communication.py
@@ -79,7 +79,7 @@ def check_condition1_thread(room_nr):
                     try:
                         
                         #rooms_dict[room_nr]["end_time"] = LESSON_HOURS[act]["ende"] ## Was tun
-                        rooms_dict[room_nr]["end_time"]=time(8, 17, 0)
+                        rooms_dict[room_nr]["end_time"]=time(8, 47, 0)
                         print(rooms_dict)
                     except:
                         print("Klappt nicht")         
