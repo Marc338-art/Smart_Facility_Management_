4b2ccc19ee4fb3d854f06fe0ed41fc7dedb833a9
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-06 06:18:52 +0000

    Delete condition1.py

--- http_requests/condition1.py
+++
@@ -1,5 +0,0 @@
-# Hier ist der Code gespeichert, der im Zustand 1 benötigt wird
-# Dieser Code steuert die Heizungsregelung basierend auf Stundenplan- und Bewegungssensordaten.
-# Wenn eine Unterrichtsstunde erkannt wird, wird die Temperatur angepasst.
-# Falls der Raum nicht belegt ist, aber Bewegung erkannt wird, kann temporär geheizt werden.
-
