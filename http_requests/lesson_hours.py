2ad08d63b0c3f29afe644e3abec1a3e135882442
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-18 06:18:23 +0000

    Edit lesson_hours.py

--- http_requests/lesson_hours.py
+++ http_requests/lesson_hours.py
@@ -23,23 +23,17 @@ LESSON_HOURS = [  ## der Stundenplan soll als sperate .py gespeichert werden um 
 ## was ist wenn es in der Pause ist(dann soll die Heizun nicht ausgeschaltet werden)
 
 rooms = [
-    "C001","C002","C003.1", "C003.2", "C003.3", "C003.4", "C003.5", "C004", "C005", "C006.1",
-    "C006.2", "C006.3", "C007", "C008", "C009", "C011", "C012",
-    "C013", "C014", "C015", "C016", "C017", "C018", "C101",
-    "C102", "C103", "C104", "C105", "C106", "C107", "C108","C109.1","C109.2","C110","C111",
-    "C112", "C113", "C114", "C115", "C116", "C117", "C117","C201","C202","C203","C204","C205",
-    "C206", "C207", "C208", "C209","C210","C211","C212","C213","C214","C215","C216","C217",
-    "C218","C219","C220","C221"
+    "C001","C002","C003.1", "C003.3",  "C004", "C005","C011","C016", "C017", "C101", "C104", "C106","C109.1","C110","C111",
+    "C112", "C115", "C116","C201","C202","C205","C208", "C209","C212","C219","C220"
 ]
  
-default_end_time = time(20, 30) 
+
 rooms_dict = {
     name: {
         "state": 1,
-        "end_time": default_end_time,
         "thread_active": False  # Neue Variable für Thread-Zustand
     }
     for name in rooms
 }
-rooms = [{"name": name, "state": 1, "end_time": default_end_time} for name in rooms]
+rooms = [{"name": name, "state": 1} for name in rooms]
 
