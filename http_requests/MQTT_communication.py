eabb1c560046fedbc2b41d67290c8deaeeafad5f
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-21 06:37:29 +0000

    Edit MQTT_communication.py

--- http_requests/MQTT_communication.py
+++ http_requests/MQTT_communication.py
@@ -138,8 +138,11 @@ def check_condition2_thread(room_nr):
 def check_wandthermostat (payload):
     name_part, temp_part = payload.split(":", 1)
     name = name_part.strip()
-    match = re.match(r"Wandthermostat_([A-Z]\d{3})_", name)
-    raum_nr = match.group(1)
+    match = re.match(r"Wandthermostat_([A-Z]\d{3})(?:_(\d+))?_", name)
+    if match:
+        raum_nr = match.group(1)
+    else:
+        raum_nr="Kein Raum"
         # Temperatur extrahieren und °C entfernen
     temp_str = temp_part.strip().replace("°C", "").strip()
     temperature = float(temp_str)
