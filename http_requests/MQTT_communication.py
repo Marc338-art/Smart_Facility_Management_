dac38adcc79aa60a8fe4f4d7fee42fbb1265f9af
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-21 06:49:59 +0000

    Edit MQTT_communication.py

--- http_requests/MQTT_communication.py
+++ http_requests/MQTT_communication.py
@@ -148,7 +148,7 @@ def check_wandthermostat (payload):
     temp_str = temp_part.strip().replace("°C", "").strip()
     temperature = float(temp_str)
     raum_nr=raum_nr.lower()
-    change_temperature(f"input_number.heating_temperature_{room_nr}", temperature)
+    change_temperature(f"input_number.heating_temperature_{raum_nr}", temperature)
     
 
 
