1e52cb0c8d30af5a7322962ac90df2767544ad14
Marc Jannik Schröder <marc.schroeder@ostfalia.de>
2025-06-21 08:09:54 +0000

    Edit MQTT_communication.py

--- http_requests/MQTT_communication.py
+++ http_requests/MQTT_communication.py
@@ -173,7 +173,7 @@ def main(payload):
     """
     print(f"Empfangener Payload: {payload}")
 
-    match = re.match(r"Wandthermostat_([A-Z]\d{3})(?:_(\d+))?_", name)
+    match = re.match(r"Bewegungsmelder_([A-Z]\d{3})(?:_(\d+))?_", payload)
 
     if match:
         raum_nr = match.group(1).lower()
